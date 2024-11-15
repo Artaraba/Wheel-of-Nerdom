4af317d08195e510bd2d06ac780d37f00703f69f
Alex Taraba <161524033+Artaraba@users.noreply.github.com>
2024-11-15 04:06:28 -0600

    Update WheelOfNerdom.cpp
    Updated .cpp file with newest version

--- WheelOfNerdom.cpp
+++ WheelOfNerdom.cpp
@@ -10,6 +10,9 @@ int main() {
 
 	// Declare and initialize question variables
 	string categoryName = "Default";
+	string question;
+	int spinOrSolve = 0;
+	
 
 	// Seed random number generator with current time (in order to get truly random number for each random number needed)
 	srand(time(0));
@@ -27,6 +30,8 @@ int main() {
 
 		// Create player object using entered name
 		Player player(playerName);
+		
+		// Push name of object created to playerVector
 		playerVector.push_back(player.name);
 
 		// Let user know player has been added
@@ -38,10 +43,41 @@ int main() {
 
 	} while (newPlayer == 'Y' || newPlayer == 'y'); // End do loop to create player objects for each player name entered
 
-	// User select category
-	cout << "Please select the category for the puzzle: Star Wars, Harry Potter";
+	// User select category, flush input stream, get category name
+	cout << endl << "Please select the category for the character: Star Wars, Harry Potter" << endl;
+	cin.ignore();
 	getline(cin, categoryName);
 
-	// If structure to choose and create secret phrase
+	// Call to category function to create question then clear screen
+	question = category(categoryName);
+
+	system("cls");
+
+	// Display character on screen as asteriks
+	cout << "Character: ";
+	for (int i = 0; i < strlen(question.c_str()); i++) {
+		if (!isblank(question[i])) {
+			cout << "*";
+		}
+		else {
+			cout << " ";
+		}
+	}
+
+	cout << endl << endl;
+
+	// Begin game by asking player to spin or solve
+	cout << endl << "Press 1 to spin and 2 to solve: ";
+	cin >> spinOrSolve;
+
+	// Call function based on user variable
+	for (int i = 0; i < playerVector.size(); i++) {
+		if (spinOrSolve == 1) {
+				playerVector[i].playerSpin(question);
+		} else {
+			cout << playerVector[i].playerSolve();
+		}
+	}
 
 	return 0;
+}
